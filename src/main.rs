07204550aa22439cbc8dab6403dc9ea527385157
Conor Gallagher <dontlisten33@gmail.com>
2024-01-23 09:43:19 -0800

    first commit

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,19 @@ use std::{path::PathBuf, vec};
 
 fn main() {
     let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let _remappings = Remapping::find_many("./ajna-core");
+    let _remappings = Remapping::find_many("hedge");
     let project = Project::builder()
         .set_auto_detect(true)
         .set_build_info(false)
         .set_no_artifacts(true)
         .set_cached(false)
+        .solc_jobs(6)
         .paths(ProjectPathsConfig {
             root: manifest_dir.clone(),
             cache: manifest_dir.join(""),
             artifacts: manifest_dir.join(""),
             build_infos: manifest_dir.join("./build-infos"),
-            sources: manifest_dir.join("./ajna-core"),
+            sources: manifest_dir.join("hedge"),
             tests: manifest_dir.join("tests"),
             scripts: manifest_dir.join("scripts"),
             libraries: vec![manifest_dir.join("lib")],
@@ -22,5 +23,5 @@ fn main() {
         })
         .build()
         .unwrap();
-    let output = project.compile().unwrap();
+    let _output = project.compile().unwrap();
 }
